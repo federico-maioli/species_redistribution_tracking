2907e8908c4ed65ddf7f945530b42eaec135d897
federico-maioli <fed.maioli@gmail.com>
2026-01-09 13:04:58 +0100

    cleaned folders

--- R/utils/write_tex.r
+++
@@ -1,8 +0,0 @@
-# rounding helper
-mround <- function(x, digits) sprintf(paste0("%.", digits, "f"), round(x, digits))
-
-# LaTeX macro writer
-write_tex <- function(x, macro, append = TRUE) {
-  paste0("\\newcommand{\\", macro, "}{", x, "}") |>
-    readr::write_lines("output/values/species_values.tex", append = append)
-}
